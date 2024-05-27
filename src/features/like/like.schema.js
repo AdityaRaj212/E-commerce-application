5fa5d31a53ec92a5080cffb303f1de1b5e2efc70
Aditya Raj <adityaraj@pop-os.localdomain>
2024-05-27 23:59:28 +0530

    [FEAT] Added mongoose middleware to likes api

--- src/features/like/like.schema.js
+++ src/features/like/like.schema.js
@@ -13,4 +13,16 @@ export const LikeSchema = mongoose.Schema({
         type: String,
         enum: ['product','category'] 
     }
+}).pre('save',(next)=>{
+    console.log("New like coming in");
+    next();
+}).post('save',(doc)=>{
+    console.log("Like is saved");
+    console.log(doc);
+}).pre('find',(next)=>{
+    console.log("Retreiving likes");
+    next();
+}).post('find',(doc)=>{
+    console.log("Find is completed");
+    console.log(doc);
 })
